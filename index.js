41aaef7db81e2b0c66a47d6a15b7b76574423da1
Fatima Lizarraga Velarde <lizarragavelardefatima@gmail.com>
2026-03-25 16:50:26 -0700

    Integración de PostgreSQL con Express

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const pool = require('./db');
 const express = require('express');
 const app = express();
 
@@ -14,6 +15,15 @@ app.get('/usuario', (req, res) => {
 
   res.json(usuario);
 });
+
+
+pool.connect()
+  .then(() => {
+    console.log('Conexión exitosa a PostgreSQL');
+  })
+  .catch((err) => {
+    console.error('Error de conexión', err);
+  });
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
