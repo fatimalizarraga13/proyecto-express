6fa757780ed283717d683e380fc6d5e51d68d2fe
Fatima Lizarraga Velarde <lizarragavelardefatima@gmail.com>
2026-04-13 16:56:15 -0700

    Actualizacion de index y db con PostgreSQL

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
-  database: 'react_express_db',
+  database: 'postgres',
   password: '123456',
   port: 5432,
 });
